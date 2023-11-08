348698ef540a23204e145ade40c029dcca7f6ece
anish <anishmaharjan17@gmail.com>
2023-11-08 18:27:56 +0545

    authentication added

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import dbConnect from "./dbConnect.js";
 import bannerRoutes from "./routes/banners.js";
 // import refreshRoutes from "./routes/refreshToken.js";
 import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
+import refreshRoutes from "./routes/refreshToken.js"
+
 
 import cors from "cors";
 
@@ -13,8 +16,10 @@ config();
 app.use(express.json());
 dbConnect();
 app.use(cors());
+
+app.use("/api", authRoutes);
 app.use("/api/users", userRoutes);
-// app.use("/api/refresh", refreshRoutes);
+app.use("/api/refresh", refreshRoutes);
 app.use("/api/banners", bannerRoutes);
 const port = process.env.PORT || 8080
 
