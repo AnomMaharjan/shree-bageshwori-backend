baf1f2bff2c674653a6f10b0022f0dafc74a86ae
Anom Maharjan <anmmzn1@gmail.com>
2023-12-13 21:54:32 +0545

    unused imports removed

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from "express";
 import { config } from "dotenv";
 import dbConnect from "./dbConnect.js";
 import bannerRoutes from "./routes/banners.js";
-import userRoutes from "./routes/users.js";
 import plansRouter from "./routes/plans.js";
 import authRoutes from "./routes/auth.js";
 import refreshRoutes from "./routes/refreshToken.js"
@@ -19,7 +18,6 @@ dbConnect();
 app.use(cors());
 
 app.use("/api", authRoutes);
-app.use("/api/users", userRoutes);
 app.use("/api/refresh", refreshRoutes);
 app.use("/api/banners", bannerRoutes);
 app.use('/api/plans', plansRouter)
