f5ba6a0c0db453b695517e96b79a561b962c471f
Anom Maharjan <anmmzn1@gmail.com>
2023-12-15 10:19:36 +0545

    blogs feature integrated and mongoose issue fixed in token generator

--- server.js
+++ server.js
@@ -6,22 +6,26 @@ import plansRouter from "./routes/plans.js";
 import authRoutes from "./routes/auth.js";
 import refreshRoutes from "./routes/refreshToken.js"
 import testimonialRouter from "./routes/testimonials.js";
-
+import blogsRouter from "./routes/blogs.js";
 import cors from "cors";
 
 const app = express();
-
+//Loads .env file contents into process.env by default.
 config();
 
+
 app.use(express.json());
-dbConnect();
 app.use(cors());
+dbConnect();
 
 app.use("/api", authRoutes);
 app.use("/api/refresh", refreshRoutes);
 app.use("/api/banners", bannerRoutes);
 app.use('/api/plans', plansRouter)
 app.use('/api/testimonials', testimonialRouter)
+app.use('/api/blogs', blogsRouter)
+
+
 const port = process.env.PORT || 8080
 
 app.listen(port, () => console.log(`listening on port ${port}`));
---
+++ utils/blogsValidator.js
@@ -0,0 +1,13 @@
+import Joi from "joi";
+
+function validateBlog(blog) {
+    const schema = Joi.object({
+        title: Joi.string().required().label('title'),
+        content: Joi.string().required().label('content'),
+        image: Joi.array().items(Joi.string())
+    })
+    return schema.validate(blog);
+}
+
+
+export default validateBlog
